5e77041e4c30bfbd05ec56e44e3f1f7124569998
E-Taratynov <el.taratynov@yandex.ru>
2025-04-22 19:02:27 +0300

    switched to vkbottle library

--- app/main.py
+++ app/main.py
@@ -1,22 +1,11 @@
-from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_api import VkApi
+from vkbottle.bot import Bot, Message
 from config import TOKEN
 
-vk_session = VkApi(token=TOKEN)
-vk = vk_session.get_api()
-longpoll = VkLongPoll(vk_session)
+bot = Bot(token=TOKEN)
 
-def echo(id, text):
-    vk.messages.send(user_id=id, message=text, random_id=0)
-
-def main_loop(longpoll: VkLongPoll):
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            if event.to_me:
-                msg = event.text.lower()
-                id = event.user_id
-                echo(id, msg)
-
+@bot.on.message()
+async def echo(message: Message):
+    await message.answer(message.text)
 
 if __name__ == "__main__":
-    main_loop(longpoll)
+    bot.run_forever()
